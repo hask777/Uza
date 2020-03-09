37a59f59b7e7d6dd1e58e73416778d32b5528d03
hask777 <haskellisp@gmail.com>
2020-03-10 00:37:33 +0300

    add accordion

--- inc/admin/assets/js/custom.js
+++ inc/admin/assets/js/custom.js
@@ -2,7 +2,8 @@
 
 jQuery(document).ready(function ($) {
   $('.admin_slider_button_text_div').hide();
-  $('.trigger').on('click', function () {
-    $('.admin_slider_button_text_div').slideToggle();
+  $('.trigger').on('click', function (e) {
+    console.log($(event.target).parent().next());
+    $(event.target).parent().next().slideToggle(); // e.target.next.slideToggle();
   });
 });
