3f35ba6bb8b28cd3eb8661a6e4ae7f9d5d602bda
hask777 <haskellisp@gmail.com>
2020-03-12 15:43:56 +0300

    rr

--- inc/admin/front.php
+++ inc/admin/front.php
@@ -1,6 +1,8 @@
 <?php
 function change_header_bgc_color()
 {
+   $options = get_option('header_bgc_init');
+
    $header_color = esc_attr(get_option('color_header_1'));
    // pr($header_color);
    ?>
--- inc/admin/uza-theme/settings/header/functions/header.php
+++ inc/admin/uza-theme/settings/header/functions/header.php
@@ -2,6 +2,7 @@
 
 function uza_header_bgc_init_callback($input){
    return $input;
+   // var_dump($input);
 }
 
 function uza_header_section_callback(){
@@ -9,18 +10,31 @@ function uza_header_section_callback(){
 }
 
 function uza_header_bgc_color_init(){
-   $header_bgc_init = get_option('header_bgc_init');
-   $checked = ( @$header_bgc_init == '1' ? 'checked' : '' );
-    pr($header_bgc_init);
-   ?>
-      <input type="checkbox" name="header_bgc_init" value="<?php echo $checked; ?>">
-   <?
+   $options = get_option('header_bgc_init');
+   // pr($options['sticky']);
+   $formats = array(
+      'transparent' => __('Прозрачность', 'uza'),
+      'sticky' => __('Фиксирование', 'uza'),
+   );
+
+   $output = '';
+   foreach($formats as $format => $value){
+      $checked = ( @$options[$format] == 1 ? 'checked' : '' );
+
+      $output .= '<label class="uza_header_options"><input type="checkbox" id="'. $format .'" name="header_bgc_init['.$format.']" value="1"'. $checked .'>'.$value.'</label>';
+   }
+
+   echo $output;
 }
 
 function uza_header_background_callback(){
    $header_color = esc_attr(get_option('color_header_1'));
-   // pr($header_color);
-   ?>
-   <input type="text" name="color_header_1" value=<?php echo $header_color; ?>>
-   <?
+   $options = get_option('header_bgc_init');
+   pr($options);
+   if(@$options['transparent'] != 1){
+      ?>
+      <input type="text" name="color_header_1" value=<?php echo $header_color; ?>>
+      <?
+   }
+
 }
